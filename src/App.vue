621fef0ad4f119aa53e425defb04decb7c72c50e
kam1lov <amirbekk@gmail.com>
2024-11-03 22:40:15 +0500

    init

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 
 <template>
    <h1>hello world</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In cum facere minus eos repellat nemo atque quia molestias nobis laboriosam debitis voluptate, sit nulla, facilis magni ab rerum inventore eligendi recusandae veritatis earum. Accusamus repellat vel, nihil laudantium totam, obcaecati officia temporibus accusantium laborum, quos nobis! Voluptatem expedita aperiam nostrum.</p>
 </template>
 
 <style scoped>
